e3d2c5d12cd3b807fc067b5d267e0ae33ca2d3e1
Baptiste Marcon <baptistemcn@icloud.com>
2021-11-08 18:47:09 +0100

    feat: AppBar v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+const dBackgroundColor = Color(0xffe8dbce);
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'House Rent',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'House Rent',
-      home: Container(),
+    return Scaffold(
+      appBar: CustomAppBar(),
+      // body: SingleChildScrollView(
+      //   child: Column(
+      //     children: const [
+      //       IntroductionSection(),
+      //     ],
+      //   ),
+      // ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [
+            IntroductionSection(),
+          ],
+        ),
+      ),
     );
+  }
+}
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  Size get preferredSize => new Size.fromHeight(50);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: const Icon(
+          Icons.menu,
+        ),
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.search,
+          ),
+        ),
+      ],
+      title: const Text("AppBar"),
+      backgroundColor: dBackgroundColor,
+    );
+  }
+}
+
+class IntroductionSection extends StatelessWidget {
+  const IntroductionSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
